a6b7ac77446d773df7dfdf82122bbf2cbe1de52f
Roddy González <roddy.gonzalez.89@gmail.com>
2017-06-27 21:13:56 -0400

    Initial commit

---
+++ gmap.rb
@@ -0,0 +1,113 @@
+class Gmap
+    def initialize(rows, cols)
+        array_length = rows * cols
+        @rows = rows
+        @cols = cols
+        @current_map = Array.new(array_length, false)
+        @next_map = Array.new(array_length, false)
+    end
+
+    def _swap()
+        temporary = @current_map
+        @current_map = @next_map
+        @next_map = temporary
+    end
+
+    def _map_indexes(i, j)
+        def _normalize_number(n, max)
+            if n < 0
+                return (max + n)
+            elsif n >= max
+                return (n % max)
+            else
+                return n
+            end
+        end
+        i_index = _normalize_number(i, @rows)
+        j_index = _normalize_number(j, @cols)
+        return ((i_index * @cols) + j_index)
+    end
+
+    def _map_indexes_denorm(i)
+        row = i / @cols
+        col = i % @cols
+        return [row, col]
+    end
+
+    def _get_cell(current, i, j)
+        array_index = _map_indexes(i, j)
+        if current
+            map = @current_map
+        else
+            map = @next_map
+        end
+        return map[array_index]
+    end
+
+    def cell(i, j)
+        return _get_cell(true, i, j)
+    end
+
+    def next_cell(i, j)
+        return _get_cell(false, i, j)
+    end
+
+    def _set_next_cell(value, i, j)
+        array_index = _map_indexes(i, j)
+        @next_map[array_index] = value
+    end
+
+    def set_cell(value, i, j)
+        array_index = _map_indexes(i, j)
+        @current_map[array_index] = value
+    end
+
+    def change_cell(i, j)
+        array_index = _map_indexes(i, j)
+        current_value = @current_map[array_index]
+        @current_map[array_index] = ! current_value
+    end
+
+    def _calc_next_value(i, j)
+        counter = 0
+        counter += 1 if cell(i + 1, j + 1)
+        counter += 1 if cell(i + 1, j)
+        counter += 1 if cell(i + 1, j - 1)
+        counter += 1 if cell(i, j + 1)
+        counter += 1 if cell(i, j - 1)
+        counter += 1 if cell(i - 1, j + 1)
+        counter += 1 if cell(i - 1, j)
+        counter += 1 if cell(i - 1, j - 1)
+        is_alive = cell(i, j)
+        can_keep_alive = (counter == 2) or (counter == 3)
+        can_be_born = (counter == 3)
+        if is_alive
+            return can_keep_alive
+        else
+            return can_be_born
+        end
+    end
+
+    def next_generation()
+        @current_map.each_with_index do |cell, counter|
+            coord = _map_indexes_denorm(counter)
+            row = coord[0]
+            col = coord[1]
+            next_value = _calc_next_value(row, col)
+            _set_next_cell(next_value, row, col)
+        end
+        _swap()
+    end
+
+    def get_diff()
+        diff = []
+        @current_map.each_with_index do |cell, counter|
+            is_diff = (@current_map[counter] != @next_map[counter])
+            if is_diff
+                coord = _map_indexes_denorm(counter)
+                diff << coord
+            end
+        end
+        return diff
+    end
+end
---
+++ tc_gmap.rb
@@ -0,0 +1,172 @@
+require_relative "gmap"
+require "test/unit"
+
+class TestGmap < Test::Unit::TestCase
+    def test_index_normalization_3x3
+        test_map = Gmap.new(3,3)
+        assert_equal(0, test_map._map_indexes(0, 0))
+        assert_equal(1, test_map._map_indexes(0, 1))
+        assert_equal(2, test_map._map_indexes(0, 2))
+        assert_equal(3, test_map._map_indexes(1, 0))
+        assert_equal(4, test_map._map_indexes(1, 1))
+        assert_equal(5, test_map._map_indexes(1, 2))
+        assert_equal(6, test_map._map_indexes(2, 0))
+        assert_equal(7, test_map._map_indexes(2, 1))
+        assert_equal(8, test_map._map_indexes(2, 2))
+    end
+
+    def test_index_normalization_2x3
+        test_map = Gmap.new(2,3)
+        assert_equal(0, test_map._map_indexes(0, 0))
+        assert_equal(1, test_map._map_indexes(0, 1))
+        assert_equal(2, test_map._map_indexes(0, 2))
+        assert_equal(3, test_map._map_indexes(1, 0))
+        assert_equal(4, test_map._map_indexes(1, 1))
+        assert_equal(5, test_map._map_indexes(1, 2))
+    end
+   
+    def test_index_normalization_3x4
+        test_map = Gmap. new(3, 4)
+        assert_equal(0, test_map._map_indexes(0, 0))
+        assert_equal(1, test_map._map_indexes(0, 1))
+        assert_equal(2, test_map._map_indexes(0, 2))
+        assert_equal(3, test_map._map_indexes(0, 3))
+        assert_equal(4, test_map._map_indexes(1, 0))
+        assert_equal(5, test_map._map_indexes(1, 1))
+        assert_equal(6, test_map._map_indexes(1, 2))
+        assert_equal(7, test_map._map_indexes(1, 3))
+        assert_equal(8, test_map._map_indexes(2, 0))
+        assert_equal(9, test_map._map_indexes(2, 1))
+        assert_equal(10, test_map._map_indexes(2, 2))
+        assert_equal(11, test_map._map_indexes(2, 3))
+    end
+    
+    def test_index_denormalization_2x3
+       test_map = Gmap.new(2, 3)
+       assert_equal([0,0], test_map._map_indexes_denorm(0))
+       assert_equal([0,1], test_map._map_indexes_denorm(1))
+       assert_equal([0,2], test_map._map_indexes_denorm(2))
+       assert_equal([1,0], test_map._map_indexes_denorm(3))
+       assert_equal([1,1], test_map._map_indexes_denorm(4))
+       assert_equal([1,2], test_map._map_indexes_denorm(5))
+    end
+
+    def test_index_denormalization_3x3
+       test_map = Gmap.new(3, 3)
+       assert_equal([0,0], test_map._map_indexes_denorm(0))
+       assert_equal([0,1], test_map._map_indexes_denorm(1))
+       assert_equal([0,2], test_map._map_indexes_denorm(2))
+       assert_equal([1,0], test_map._map_indexes_denorm(3))
+       assert_equal([1,1], test_map._map_indexes_denorm(4))
+       assert_equal([1,2], test_map._map_indexes_denorm(5))
+       assert_equal([2,0], test_map._map_indexes_denorm(6))
+       assert_equal([2,1], test_map._map_indexes_denorm(7))
+       assert_equal([2,2], test_map._map_indexes_denorm(8))
+    end
+
+    def test_index_denormalization_3x4
+       test_map = Gmap.new(3, 4)
+       assert_equal([0,0], test_map._map_indexes_denorm(0))
+       assert_equal([0,1], test_map._map_indexes_denorm(1))
+       assert_equal([0,2], test_map._map_indexes_denorm(2))
+       assert_equal([0,3], test_map._map_indexes_denorm(3))
+       assert_equal([1,0], test_map._map_indexes_denorm(4))
+       assert_equal([1,1], test_map._map_indexes_denorm(5))
+       assert_equal([1,2], test_map._map_indexes_denorm(6))
+       assert_equal([1,3], test_map._map_indexes_denorm(7))
+       assert_equal([2,0], test_map._map_indexes_denorm(8))
+       assert_equal([2,1], test_map._map_indexes_denorm(9))
+       assert_equal([2,2], test_map._map_indexes_denorm(10))
+       assert_equal([2,3], test_map._map_indexes_denorm(11))
+    end
+
+
+    def test_set_cell_3x3
+        test_map = Gmap.new(3, 3)
+        3.times do |i|
+            3.times do |j|
+                assert_equal(false, test_map.cell(i, j))
+            end
+        end
+        test_map.set_cell(true, 0, 0)
+        assert_equal(true, test_map.cell(0, 0))
+        test_map.set_cell(false, 0, 0)
+        assert_equal(false, test_map.cell(0, 0))
+    end
+
+    def test_blinker_4x4
+        blinker = [
+            [false, true, false, false],
+            [false, true, false, false],
+            [false, true, false, false,],
+            [false, false, false, false]
+        ]
+
+        post_blinker = [
+            [false, false, false, false],
+            [true, true, true, false],
+            [false, false, false, false],
+            [false, false, false, false]
+        ]
+        test_map = Gmap.new(4, 4)
+        4.times do |i|
+            4.times do |j|
+                test_map.set_cell(blinker[i][j], i, j)
+            end
+        end
+        test_map.next_generation()
+        4.times do |i|
+            4.times do |j|
+                assert_equal(post_blinker[i][j], test_map.cell(i, j))
+            end
+        end
+        test_map.next_generation()
+        4.times do |i|
+            4.times do |j|
+                assert_equal(blinker[i][j], test_map.cell(i, j))
+            end
+        end
+    end
+
+    def test_swap_3x3
+        test_map = Gmap.new(3, 3)
+        test_map.set_cell(true, 0, 0)
+        assert_equal(true, test_map.cell(0, 0))
+        assert_equal(false, test_map.cell(0, 1))
+        assert_equal(false, test_map.cell(0, 2))
+        assert_equal(false, test_map.cell(1, 0))
+        assert_equal(false, test_map.cell(1, 1))
+        assert_equal(false, test_map.cell(1, 2))
+        assert_equal(false, test_map.cell(2, 0))
+        assert_equal(false, test_map.cell(2, 1))
+        assert_equal(false, test_map.cell(2, 2))
+        test_map._swap
+        assert_equal(false, test_map.cell(0, 0))
+        assert_equal(false, test_map.cell(0, 1))
+        assert_equal(false, test_map.cell(0, 2))
+        assert_equal(false, test_map.cell(1, 0))
+        assert_equal(false, test_map.cell(1, 1))
+        assert_equal(false, test_map.cell(1, 2))
+        assert_equal(false, test_map.cell(2, 0))
+        assert_equal(false, test_map.cell(2, 1))
+        assert_equal(false, test_map.cell(2, 2))
+        test_map._swap
+        assert_equal(true, test_map.cell(0, 0))
+        assert_equal(false, test_map.cell(0, 1))
+        assert_equal(false, test_map.cell(0, 2))
+        assert_equal(false, test_map.cell(1, 0))
+        assert_equal(false, test_map.cell(1, 1))
+        assert_equal(false, test_map.cell(1, 2))
+        assert_equal(false, test_map.cell(2, 0))
+        assert_equal(false, test_map.cell(2, 1))
+        assert_equal(false, test_map.cell(2, 2))
+    end
+
+    def test_next_cell_2x2
+        test_map = Gmap.new(2, 2)
+        test_map.set_cell(true, 0, 0)
+        assert_equal(true, test_map.cell(0, 0))
+        assert_equal(false, test_map.next_cell(0, 0))
+    end
+end
+
